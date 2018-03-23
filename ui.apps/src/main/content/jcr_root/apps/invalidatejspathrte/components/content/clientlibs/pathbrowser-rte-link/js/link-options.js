19fc7a7792af291ad6dccd2b807944635f848456
Mohammed Shaji Ahmed <shajiahmed@gmail.com>
2018-03-23 17:00:55 -0400

    added foundation-content-loaded event

--- ui.apps/src/main/content/jcr_root/apps/invalidatejspathrte/components/content/clientlibs/pathbrowser-rte-link/js/link-options.js
+++ ui.apps/src/main/content/jcr_root/apps/invalidatejspathrte/components/content/clientlibs/pathbrowser-rte-link/js/link-options.js
@@ -3,6 +3,9 @@
 	$document.on("dialog-ready", function() {
        $(".js-coral-pathbrowser-input").removeClass("js-coral-pathbrowser-input");
     });
+    $(document).on("foundation-contentloaded", function(e) {
+   		$(".js-coral-pathbrowser-input").removeClass("js-coral-pathbrowser-input");
+	});
 	var _ = window._, Class = window.Class, CUI = window.CUI, RTE_LINK_DIALOG = "rtelinkdialog";
     // Remove validation class from the pathbrowser
 	 $(".js-coral-pathbrowser-input").removeClass("js-coral-pathbrowser-input");
